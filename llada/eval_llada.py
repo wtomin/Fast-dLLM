d128cef3d4f8044f90d8cf0178411312b2176edd
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-09-11 16:44:20 +0800

    support dual cache

--- llada/eval_llada.py
+++ llada/eval_llada.py
@@ -34,7 +34,7 @@ from tqdm import tqdm
 import os
 from transformers import AutoTokenizer, AutoModel, AutoConfig
 from generate import generate, generate_with_prefix_cache, generate_with_dual_cache
-from generate_dynamic_block import generate_with_prefix_dynamic_block_length
+from generate_dynamic_block import generate_with_prefix_dynamic_block_length, generate_with_dual_dynamic_block_length
 from model.modeling_llada import LLaDAModelLM
 import json
 import time
@@ -341,7 +341,11 @@ class LLaDAEvalHarness(LM):
             input_ids = batched_input_ids
             if self.use_cache:
                 if self.dual_cache:
-                    generated_answer, nfe = generate_with_dual_cache(self.model, input_ids, steps=self.steps, gen_length=self.gen_length, block_length=self.block_length, 
+                    if self.dynamic_block_length:
+                        generated_answer, nfe = generate_with_dual_dynamic_block_length(self.model, input_ids, steps=self.steps, gen_length=self.gen_length,
+                                        temperature=0, remasking=self.remasking, mask_id=self.mask_id, threshold=self.threshold, factor=self.factor)
+                    else:
+                        generated_answer, nfe = generate_with_dual_cache(self.model, input_ids, steps=self.steps, gen_length=self.gen_length, block_length=self.block_length, 
                                         temperature=0, remasking=self.remasking, mask_id=self.mask_id, threshold=self.threshold, factor=self.factor)
                 else:
                     if self.dynamic_block_length:
--- llada/generate_dynamic_block.py
+++ llada/generate_dynamic_block.py
@@ -17,6 +17,86 @@ def powers_of_two_in_range(min_block_length, max_block_length):
         n += 1
     return result
 
+@ torch.no_grad()
+def generate_with_dual_dynamic_block_length(model, prompt, steps=128, gen_length=128, block_length=128, temperature=0.,
+            min_block_length=4, max_block_length=64,
+            remasking='low_confidence', mask_id=126336, threshold=None, factor=None):
+    '''
+    Args:
+        model: Mask predictor.
+        prompt: A tensor of shape (1, L).
+        steps: Sampling steps, less than or equal to gen_length.
+        gen_length: Generated answer length.
+        min_block_length: Minimal block length, less than gen_length.
+        max_block_length: Maximal block length, less than gen_length.
+        temperature: Categorical distribution sampling temperature.
+        cfg_scale: Unsupervised classifier-free guidance scale.
+        remasking: Remasking strategy. 'low_confidence' or 'random'.
+        mask_id: The toke id of [MASK] is 126336.
+    '''
+    x = torch.full((prompt.shape[0], prompt.shape[1] + gen_length), mask_id, dtype=torch.long).to(model.device)
+    x[:, :prompt.shape[1]] = prompt.clone()
+    assert gen_length % min_block_length == 0 and gen_length % max_block_length == 0, "gen_length must be divisible by min_block_length and max_block_length"
+    valid_block_lengths = powers_of_two_in_range(min_block_length, max_block_length)
+    steps = 1
+
+    nfe = 0  
+    current_block_start = prompt.shape[1]
+    
+    while current_block_start < x.shape[1]:
+        output = model(x, use_cache=True)
+        past_key_values = output.past_key_values
+
+        # run single model forward with prefix cache with the largest block length, to determine the block length
+        logits = output.logits
+        logits_with_noise = add_gumbel_noise(logits, temperature=temperature)
+        x0 = torch.argmax(logits_with_noise, dim=-1) # b, l     
+        p = F.softmax(logits, dim=-1)
+        x0_p = torch.squeeze(torch.gather(p, dim=-1, index=torch.unsqueeze(x0, -1)), -1) # b, l
+        for block_length in valid_block_lengths:
+            avg_confidence = x0_p[:, :block_length].mean(dim=-1)
+            current_block_end = current_block_start + block_length
+            if current_block_end >= x.shape[1]:
+                break
+            if avg_confidence.mean() < 0.2:
+                break
+
+        logger.info(f"Block length: {block_length}, avg confidence: {avg_confidence.mean()}")
+
+        block_mask_index = (x[:, current_block_start:current_block_end] == mask_id)
+        num_transfer_tokens = get_num_transfer_tokens(block_mask_index, steps)
+
+        mask_index = (x == mask_id)
+        mask_index[:, current_block_end:] = 0
+        if factor is None:
+            x0, transfer_index = get_transfer_index(output.logits, temperature, remasking, mask_index, x, num_transfer_tokens[:, 0] if threshold is None else None, threshold)
+        else:
+            x0, transfer_index = get_transfer_index_dynamic(output.logits, temperature, remasking, mask_index, x, None, factor)
+        x[transfer_index] = x0[transfer_index]
+        nfe += 1
+
+        i = 1
+        replace_position = torch.zeros_like(x, dtype=torch.bool)
+        replace_position[:, current_block_start:current_block_end] = 1
+        while True:
+            if (x[:, current_block_start:current_block_end] == mask_id).sum() == 0:
+                current_block_start = current_block_end
+                break
+            nfe += 1
+            mask_index = (x[:, current_block_start:current_block_end] == mask_id)
+            # cache position is the position between current_block_start and current_block_end
+            logits = model(x[:, current_block_start:current_block_end], past_key_values=past_key_values, use_cache=True, replace_position=replace_position).logits
+
+            if factor is None:
+                x0, transfer_index = get_transfer_index(logits, temperature, remasking, mask_index, 
+                                                x[:, current_block_start:current_block_end], num_transfer_tokens[:, i] if threshold is None else None, threshold)
+            else:
+                x0, transfer_index = get_transfer_index_dynamic(logits, temperature, remasking, mask_index, 
+                                                x[:, current_block_start:current_block_end], None, factor)
+            x[:, current_block_start:current_block_end][transfer_index] = x0[transfer_index]
+            i += 1
+
+    return x, nfe
 
 
 @torch.no_grad() 
