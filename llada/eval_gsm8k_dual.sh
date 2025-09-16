02bc702cac020fc3855c76b9a2ce9fb9aad2d416
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-09-16 17:25:46 +0800

    sub block ratio evaluation

--- llada/eval_gsm8k_dual.sh
+++ llada/eval_gsm8k_dual.sh
@@ -9,6 +9,7 @@ num_fewshot=5
 steps=$((length / block_length))
 factor=1.0
 model_path='GSAI-ML/LLaDA-8B-Instruct'
+sub_block_ratio=0.5
 # You can change the model path to LLaDA-1.5 by setting model_path='GSAI-ML/LLaDA-1.5'
 
 
@@ -30,4 +31,10 @@ python eval_llada.py --tasks ${task} --num_fewshot ${num_fewshot} \
 python eval_llada.py --tasks ${task} --num_fewshot ${num_fewshot} \
 --confirm_run_unsafe_code --model llada_dist \
 --model_args model_path=${model_path},gen_length=${length},steps=${steps},use_cache=True,dual_cache=True,dynamic_block_length=True,factor=${factor},show_speed=True \
+--limit 50 
+
+# dynamic block length+ sub_block_ratio +parallel
+python eval_llada.py --tasks ${task} --num_fewshot ${num_fewshot} \
+--confirm_run_unsafe_code --model llada_dist \
+--model_args model_path=${model_path},gen_length=${length},steps=${steps},use_cache=True,dual_cache=True,dynamic_block_length=True,factor=${factor},sub_block_ratio=${sub_block_ratio},show_speed=True \
 --limit 50 
